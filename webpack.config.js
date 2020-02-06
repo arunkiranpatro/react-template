21783ab9a858671b1c8186cc09ecf45ab3d3df60
Arun Kiran Patro <arunpatro494@gmail.com>
2020-02-06 07:43:00 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: "./index.js",
+  output: { path: path.resolve(__dirname, "dist"), filename: "bundle.js" },
+  mode: "development",
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          //MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  devServer: {
+    inline: true,
+    contentBase: "./dist",
+    port: 3000
+  },
+  devtool:'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'styles.css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
