cc3c0b7099e24e5d2fd608abb382f2cce6b06f3e
Arun Kiran Patro <arunpatro494@gmail.com>
2020-02-11 13:30:49 +0530

    fixed

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
-import DCReducer from "./DCReducer";
 
 export default combineReducers({
-  errors: errorReducer,
-  multipleDC: DCReducer
+  errors: errorReducer
 });
